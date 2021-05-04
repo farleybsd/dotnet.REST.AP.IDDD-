7bf75621896567d5b5b64dccd88100cc81bdebfd
farleybsd <farley.t.i@hotmail.com>
2021-05-04 14:45:01 -0300

    Model IOC

---
+++ RestApiModeloTdd/RestApiModeloTdd.Infrastruture/CrossCutting/IOC/ModeIOC.cs
@@ -0,0 +1,12 @@
+﻿using Autofac;
+
+namespace RestApiModeloTdd.Infrastruture.CrossCutting.IOC
+{
+    public class ModeIOC : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            ConfigurationIOC.Load(builder);
+        }
+    }
+}
