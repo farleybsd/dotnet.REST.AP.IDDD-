fedc84b33266bddade76e0e3bdbc46becb2ef6b4
farleybsd <farley.t.i@hotmail.com>
2021-05-04 10:23:15 -0300

    ADD DTO CLIENTE PRODUTO

---
+++ RestApiModeloTdd/RestApiModeloTdd.Aplication/Dtos/ClienteDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace RestApiModeloTdd.Aplication.Dtos
+{
+    public class ClienteDTO
+    {
+        public int? Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ RestApiModeloTdd/RestApiModeloTdd.Aplication/Dtos/ProdutoDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace RestApiModeloTdd.Aplication.Dtos
+{
+    public class ProdutoDTO
+    {
+        public int? Id { get; set; }
+        public string Nome { get; set; }
+        public Decimal Valor { get; set; }
+    }
+}
