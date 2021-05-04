c85b3fdc077bee1b6f922a10339de98b00eb9c39
farleybsd <farley.t.i@hotmail.com>
2021-05-04 10:13:01 -0300

    Serice Produto Cliente Finish

---
+++ RestApiModeloTdd/RestApiModeloTdd.Domain.Services/ServiceCliente.cs
@@ -0,0 +1,17 @@
+﻿using RestApiModeloTdd.Domain.Core.Interfaces.Repositorys;
+using RestApiModeloTdd.Domain.Core.Interfaces.Services;
+using RestApiModeloTdd.Domain.Entitys;
+
+namespace RestApiModeloTdd.Domain.Services
+{
+    public class ServiceCliente : ServiceBase<Cliente>, IServiceCliente
+    {
+        private readonly IRepositoryCliente repositoryCliente;
+
+        public ServiceCliente(IRepositoryCliente repositoryCliente)
+            : base(repositoryCliente)
+        {
+            this.repositoryCliente = repositoryCliente;
+        }
+    }
+}
---
+++ RestApiModeloTdd/RestApiModeloTdd.Domain.Services/ServiceProduto.cs
@@ -0,0 +1,17 @@
+﻿using RestApiModeloTdd.Domain.Core.Interfaces.Repositorys;
+using RestApiModeloTdd.Domain.Core.Interfaces.Services;
+using RestApiModeloTdd.Domain.Entitys;
+
+namespace RestApiModeloTdd.Domain.Services
+{
+    public class ServiceProduto : ServiceBase<Produto>, IServiceProduto
+    {
+        private readonly IRepositoryProduto repositoryProduto;
+
+        public ServiceProduto(IRepositoryProduto repositoryProduto)
+            : base(repositoryProduto)
+        {
+            this.repositoryProduto = repositoryProduto;
+        }
+    }
+}
