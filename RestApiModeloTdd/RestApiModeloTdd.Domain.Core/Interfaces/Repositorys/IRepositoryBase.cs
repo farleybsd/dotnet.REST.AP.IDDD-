3505d3e09614373aa3c8b0bb2ba027193f52ef3d
farleybsd <farley.t.i@hotmail.com>
2021-05-04 00:58:47 -0300

    Add IRepositoryBase

---
+++ RestApiModeloTdd/RestApiModeloTdd.Domain.Core/Interfaces/Repositorys/IRepositoryBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace RestApiModeloTdd.Domain.Core.Interfaces.Repositorys
+{
+    public interface IRepositoryBase<TEntity> where TEntity : class
+    {
+        void Add(TEntity obj);
+        void Update(TEntity obj);
+        void Remove(TEntity obj);
+        IEnumerable<TEntity> GetAll();
+        TEntity GetById(int id);
+    }
+}
