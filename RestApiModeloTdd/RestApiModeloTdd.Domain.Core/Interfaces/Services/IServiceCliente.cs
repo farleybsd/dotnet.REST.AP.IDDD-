8d85b1d3e568562e1f0e0ddb48e2709d2e690185
farleybsd <farley.t.i@hotmail.com>
2021-05-04 10:01:24 -0300

    Add Service Cliente Produto

---
+++ RestApiModeloTdd/RestApiModeloTdd.Domain.Core/Interfaces/Services/IServiceCliente.cs
@@ -0,0 +1,8 @@
+﻿using RestApiModeloTdd.Domain.Entitys;
+
+namespace RestApiModeloTdd.Domain.Core.Interfaces.Services
+{
+    public interface IServiceCliente : IServiceBase<Cliente>
+    {
+    }
+}
