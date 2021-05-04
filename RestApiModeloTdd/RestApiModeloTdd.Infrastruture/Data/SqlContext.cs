36551a4438ab80a263c40640427b368109ffff16
farleybsd <farley.t.i@hotmail.com>
2021-05-04 00:50:39 -0300

    Add SqlContext

---
+++ RestApiModeloTdd/RestApiModeloTdd.Infrastruture/Data/SqlContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestApiModeloTdd.Domain.Entitys;
+using System;
+using System.Linq;
+
+namespace RestApiModeloTdd.Infrastruture.Data
+{
+    public class SqlContext : DbContext
+    {
+        public SqlContext()
+        {
+
+        }
+
+        public SqlContext(DbContextOptions<SqlContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Cliente> clientes { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public override int SaveChanges()
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+                }
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("DataCadastro").IsModified = false;
+                }
+            }
+            return base.SaveChanges();
+        }
+    }
+}
